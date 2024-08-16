2de77461b468394b6b71876de974767967e39aed
Teksphere <160017075+Teksphere@users.noreply.github.com>
2024-08-16 08:34:23 +0100

    Update Installation-script.sh

--- Installation-script.sh
+++ Installation-script.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 sudo apt update -y
-sudo touch /etc/apt/keyrings/adoptium.asc
-sudo wget -O /etc/apt/keyrings/adoptium.asc https://packages.adoptium.net/artifactory/api/gpg/key/public
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update -y
-sudo apt install temurin-17-jdk -y
+sudo apt install openjdk-17-jre-headless -y
 /usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-                  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-                  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-                              /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
 sudo apt-get install jenkins -y
-sudo systemctl start jenkins
-sudo systemctl status jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 #Install docker
 sudo apt install docker.io -y
