8f893cb6bc301bfd6481f76eaf57370f0db037be
Ibukun Soetan <160017075+h3llo-world-33@users.noreply.github.com>
2024-11-07 12:16:57 +0100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "teksphere-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "hello-mrcloudbook777" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
